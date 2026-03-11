4dee063cc33bcec1be29c0423142f275fadb002a
ts-gunner <yang995854654@outlook.com>
2026-03-11 19:13:52 +0800

    新增客户数据页面

--- frontend-admin/src/pages/CrmValueManagement/index.tsx
+++ frontend-admin/src/pages/CrmValueManagement/index.tsx
@@ -1,9 +1,28 @@
+import { Tabs } from 'antd';
 import React from 'react'
 
 export default function CrmValueManagementPage() {
+  
   return (
     <div>
-      CrmValueManagementPage
+       <Tabs
+    defaultActiveKey="2"
+    items={[0, 1].map((Icon, i) => {
+      const id = String(i + 1);
+      return {
+        key: id,
+        label: `Tab ${id}`,
+        children: `Tab ${id}`,
+      };
+    })}
+  />
     </div>
+  )
+}
+
+const CrmValueTable:React.FC<{}> = () => {
+  return (
+    <>
+    </>
   )
 }
