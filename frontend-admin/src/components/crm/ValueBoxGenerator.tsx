fc64171c052acd7d3c4a2d1e055e950653898fa3
ts-gunner <yang995854654@outlook.com>
2026-03-17 15:15:05 +0800

    修复bool类型的字段选择false时无法显示

--- frontend-admin/src/components/crm/ValueBoxGenerator.tsx
+++ frontend-admin/src/components/crm/ValueBoxGenerator.tsx
@@ -1,34 +1,34 @@
-import { CrmDataTypeEnum } from '@/constants/enums';
-import { DatePicker, Input, InputNumber, Select } from 'antd';
-import dayjs from 'dayjs';
+import { CrmDataTypeEnum } from "@/constants/enums";
+import { DatePicker, Input, InputNumber, Select } from "antd";
+import dayjs from "dayjs";
 
-export default function ValueBoxGenerator({ field, value, onChange }: { field: API.CrmEntityFieldVo, [key: string]: any }) {
+export default function ValueBoxGenerator({ field, value, onChange }: { field: API.CrmEntityFieldVo; [key: string]: any }) {
   const { dataType, fieldName, options } = field;
 
   switch (dataType) {
     case CrmDataTypeEnum.Number:
-      return <InputNumber value={value}  onChange={onChange} className="w-full" placeholder={`请输入${fieldName}`} />;
+      return <InputNumber value={value} onChange={onChange} className="w-full" placeholder={`请输入${fieldName}`} />;
     case CrmDataTypeEnum.Boolean:
       return (
         <Select
-          value={value || "未选择"}
+          value={value != null ? value : "未选择"}
           allowClear
           onChange={onChange}
           placeholder={`请选择${fieldName}`}
           options={[
             {
               label: "是",
-            value: true
+              value: true,
             },
             {
               label: "否",
-              value: false
-            }
+              value: false,
+            },
           ]}
         ></Select>
       );
     case CrmDataTypeEnum.Picker:
-      const selectOptions = options ? options.split(",") : []
+      const selectOptions = options ? options.split(",") : [];
       return (
         <Select
           value={value || "未选择"}
@@ -43,10 +43,17 @@ export default function ValueBoxGenerator({ field, value, onChange }: { field: A
       );
     case CrmDataTypeEnum.Date:
       const dateValue = value ? dayjs(value) : undefined;
-      return <DatePicker value={dateValue}  onChange={(val) => {
-        onChange(val?.format("YYYY-MM-DD"))
-      }} className="w-full" format={"YYYY-MM-DD"} />;
+      return (
+        <DatePicker
+          value={dateValue}
+          onChange={(val) => {
+            onChange(val?.format("YYYY-MM-DD"));
+          }}
+          className="w-full"
+          format={"YYYY-MM-DD"}
+        />
+      );
     default:
-      return <Input value={value}  onChange={onChange} placeholder={`请输入${fieldName}`} />;
+      return <Input value={value} onChange={onChange} placeholder={`请输入${fieldName}`} />;
   }
 }
--- frontend-admin/src/pages/CrmValueManagement/UpdateEntityValueModal.tsx
+++ frontend-admin/src/pages/CrmValueManagement/UpdateEntityValueModal.tsx
@@ -1,74 +1,64 @@
-import ValueBoxGenerator from '@/components/crm/ValueBoxGenerator';
-import { Form, Modal} from 'antd';
-import React, { useEffect, useState } from 'react'
-
-
+import ValueBoxGenerator from "@/components/crm/ValueBoxGenerator";
+import { Form, Modal } from "antd";
+import { useEffect, useState } from "react";
 
 type ModalProps = {
-    modalOpen: boolean;
-    handleModalOpen: any;
-    onSubmit: (data: any) => Promise<void>;
-    value: any;
-    fieldList: API.CrmEntityFieldVo[];
+  modalOpen: boolean;
+  handleModalOpen: any;
+  onSubmit: (data: any) => Promise<void>;
+  value: any;
+  fieldList: API.CrmEntityFieldVo[];
 };
 
-
 export default function UpdateEntityValueModal({ modalOpen, handleModalOpen, fieldList, onSubmit, value }: ModalProps) {
-    const [btnLoading, setBtnLoading] = useState<boolean>(false);
-    const [form] = Form.useForm();
-     useEffect(() => {
-        if (modalOpen && value) {
-          form.setFieldsValue(value);
-        } else if (!modalOpen) {
-          form.resetFields();
+  const [btnLoading, setBtnLoading] = useState<boolean>(false);
+  const [form] = Form.useForm();
+  useEffect(() => {
+    if (modalOpen && value) {
+      form.setFieldsValue(value);
+    } else if (!modalOpen) {
+      form.resetFields();
+    }
+  }, [modalOpen, value]);
+
+  return (
+    <Modal
+      title="更新客户数据"
+      open={modalOpen}
+      onCancel={() => handleModalOpen(false)}
+      className=""
+      width={"70%"}
+      confirmLoading={btnLoading}
+      destroyOnHidden
+      onOk={async () => {
+        try {
+          setBtnLoading(true);
+          const data = await form.validateFields();
+          await onSubmit(data);
+        } finally {
+          setBtnLoading(false);
         }
-      }, [modalOpen, value]);
- 
-    return (
-        <Modal
-            title="更新客户数据"
-            open={modalOpen}
-            onCancel={() => handleModalOpen(false)}
-            className=""
-            width={"70%"}
-            confirmLoading={btnLoading}
-            destroyOnHidden
-            onOk={async () => {
-                try {
-                    setBtnLoading(true);
-                    const data = await form.validateFields();
-                    await onSubmit(data);
-                } finally {
-                    setBtnLoading(false);
-
-                }
-
-            }}
-        >
-            <div className='h-[70vh] overflow-y-auto custom-scrollbar p-3'>
-  <Form
-                form={form}
-                layout="vertical"
-                className="mt-4"
-            >
-                <div className="grid grid-cols-2 gap-x-4">
-                    {fieldList
-                        ?.sort((a, b) => (a.sortOrder || 0) - (b.sortOrder || 0))
-                        .map((field) => (
-                            <Form.Item
-                                key={field.fieldKey}
-                                name={field.fieldKey}
-                                label={field.fieldName}
-                                rules={[{ required: field.isRequired, message: `${field.fieldName}是必填项` }]}
-                                className="col-span-1"
-                            >
-                              <ValueBoxGenerator field={field}/>
-                            </Form.Item>
-                        ))}
-                </div>
-            </Form>
-            </div>
-          
-        </Modal>
-    );
+      }}
+    >
+      <div className="h-[70vh] overflow-y-auto custom-scrollbar p-3">
+        <Form form={form} layout="vertical" className="mt-4">
+          <div className="grid grid-cols-2 gap-x-4">
+            {fieldList
+              ?.sort((a, b) => (a.sortOrder || 0) - (b.sortOrder || 0))
+              .map((field) => (
+                <Form.Item
+                  key={field.fieldKey}
+                  name={field.fieldKey}
+                  label={field.fieldName}
+                  rules={[{ required: field.isRequired, message: `${field.fieldName}是必填项` }]}
+                  className="col-span-1"
+                >
+                  <ValueBoxGenerator field={field} />
+                </Form.Item>
+              ))}
+          </div>
+        </Form>
+      </div>
+    </Modal>
+  );
 }
