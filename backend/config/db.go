2b24df5267d0a43db3e6f059ba2292f55356fe39
ts-gunner <yang995854654@outlook.com>
2026-03-19 18:51:14 +0800

    更新配置结构体

--- backend/config/db.go
+++ backend/config/db.go
@@ -4,11 +4,11 @@ type DsnProvider interface {
 	Dsn() string
 }
 type CommonDB struct {
-	Path         string `mapstructure:"path" json:"path" yaml:"path"`             // 地址 | 路径
-	Port         string `mapstructure:"port" json:"port" yaml:"port"`             // 端口号
-	DbName       string `mapstructure:"db-name" json:"db-name" yaml:"db-name"`    // 数据库名
-	Username     string `mapstructure:"username" json:"username" yaml:"username"` // 用户名
-	Password     string `mapstructure:"password" json:"password" yaml:"password"` // 密码
-	MaxIdleConns int    `json:"max-idle-conns" yaml:"max-idle-conns"`             // 空闲中最大的连接数
-	MaxOpenConns int    `json:"max-open-conns" yaml:"max-open-conns"`             // 打开数据库的最大连接数
+	Path         string `mapstructure:"path" json:"path" yaml:"path"`                               // 地址 | 路径
+	Port         string `mapstructure:"port" json:"port" yaml:"port"`                               // 端口号
+	DbName       string `mapstructure:"db-name" json:"db-name" yaml:"db-name"`                      // 数据库名
+	Username     string `mapstructure:"username" json:"username" yaml:"username"`                   // 用户名
+	Password     string `mapstructure:"password" json:"password" yaml:"password"`                   // 密码
+	MaxIdleConns int    `mapstructure:"max-idle-conns" json:"max-idle-conns" yaml:"max-idle-conns"` // 空闲中最大的连接数
+	MaxOpenConns int    `mapstructure:"max-open-conns" json:"max-open-conns" yaml:"max-open-conns"` // 打开数据库的最大连接数
 }
--- backend/config/store.go
+++ backend/config/store.go
@@ -1,7 +1,38 @@
 package config
 
-type StoreConfig struct {
-	StoreType       string `mapstructure:"store-type" json:"store-type" yaml:"store-type"`                      // 存储类型， local | aliyun | minio
+type BaseStoreField struct {
 	AccessPrefixUrl string `mapstructure:"access-prefix-url" json:"access-prefix-url" yaml:"access-prefix-url"` // 访问资源的前缀路径
 	BaseRelPath     string `mapstructure:"base-rel-path" json:"base-rel-path" yaml:"base-rel-path"`             // 存储的相对路径
+}
+
+type BaseCloudField struct {
+	AccessKeyId     string `mapstructure:"access-key-id" json:"access-key-id" yaml:"access-key-id"`
+	AccessKeySecret string `mapstructure:"access-key-secret" json:"access-key-secret" yaml:"access-key-secret"`
+}
+
+type StoreConfig struct {
+	StoreType string         `mapstructure:"store-type" json:"store-type" yaml:"store-type"` // 默认存储类型， superbed | local | aliyun | minio
+	SuperBed  SuperBedConfig `mapstructure:"super-bed" json:"super-bed" yaml:"super-bed"`    // 聚合图床的存储
+	Aliyun    AliyunConfig   `mapstructure:"aliyun" json:"aliyun" yaml:"aliyun"`             // 阿里云存储
+	Tencent   TencentConfig  `mapstructure:"tencent" json:"tencent" yaml:"tencent"`          // 腾讯云存储
+	Minio     MinioConfig    `mapstructure:"minio" json:"minio" yaml:"minio"`                // minio对象存储
+
+}
+
+type SuperBedConfig struct {
+	BaseStoreField `mapstructure:",squash" yaml:",inline"`
+}
+
+type AliyunConfig struct {
+	BaseCloudField `mapstructure:",squash" yaml:",inline"`
+	BaseStoreField `mapstructure:",squash" yaml:",inline"`
+}
+
+type TencentConfig struct {
+	BaseCloudField `mapstructure:",squash" yaml:",inline"`
+	BaseStoreField `mapstructure:",squash" yaml:",inline"`
+}
+
+type MinioConfig struct {
+	BaseStoreField `mapstructure:",squash" yaml:",inline"`
 }
