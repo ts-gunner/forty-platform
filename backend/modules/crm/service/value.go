bbd273aa3636634a893a81066c74f0ea0da7baeb
ts-gunner <yang995854654@outlook.com>
2026-04-06 23:01:53 +0800

    添加注释

--- backend/modules/crm/service/value.go
+++ backend/modules/crm/service/value.go
@@ -473,7 +473,7 @@ func (EntityValueService) HandleUploadExcel(ctx context.Context, req request.Upl
 			{Name: "customer_name"},
 			{Name: "entity_id"},
 			{Name: "user_id"},
-		},                                                                               // 判重唯一键，对应数据库字段名
+		}, // 判重唯一键，对应数据库字段名
 		DoUpdates: clause.AssignmentColumns([]string{"remark", "values", "updater_id"}), // 存在时更新的字段
 	}).Create(&valueData).Error; err != nil {
 		return fmt.Errorf("创建失败：%v", err)
