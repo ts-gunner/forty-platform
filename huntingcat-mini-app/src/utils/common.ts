b2cbad8343638d38bab4643b5ea7c7cf922e5b54
ts-gunner <yang995854654@outlook.com>
2026-05-19 11:39:36 +0800

    优化登录过期时反复跳转登录页

--- huntingcat-mini-app/src/utils/common.ts
+++ huntingcat-mini-app/src/utils/common.ts
@@ -4,7 +4,17 @@ import { ApiResult } from "@/typing";
 import Taro from "@tarojs/taro";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-
+let isRun = false;
+const executeFunction = (func: () => void) => {
+  if (!isRun) {
+    isRun = true;
+    func();
+    // 2秒后重置状态，允许下一次报错
+    setTimeout(() => {
+      isRun = false;
+    }, 2000);
+  }
+};
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -25,13 +35,14 @@ export function handleResponse<T = any>({
       return;
     }
     if (resp.code === 401) {
-      Notify.fail("登录过期");
-      store.dispatch.routerModel.navigateTo({url: ROUTERS.login });
-    } 
-    if (resp.code === 500) {
-      Notify.fail(resp.msg)
+      executeFunction(() => {
+        Notify.fail("登录过期");
+        store.dispatch.routerModel.navigateTo({ url: ROUTERS.login });
+      });
     }
-    else if (resp.code === 200) {
+    if (resp.code === 500) {
+      Notify.fail(resp.msg);
+    } else if (resp.code === 200) {
       onSuccess?.(resp.data as T);
     } else {
       onError?.(resp.data as T);
@@ -55,6 +66,6 @@ export class Notify {
     store.dispatch.notificationModel.notifyFail(message);
   }
   static clear() {
-    store.dispatch.notificationModel.hideNotify()
+    store.dispatch.notificationModel.hideNotify();
   }
 }
